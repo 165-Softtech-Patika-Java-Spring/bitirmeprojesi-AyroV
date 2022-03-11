6d9f9651983cc385f9cc28c1f93a6881cc22ed99
Mertcan Elçi <mertcanelci@outlook.com>
2022-03-11 11:50:18 +0300

    Project Initialized

---
+++ src/test/java/com/softtech/webapp/BitirmeprojesiAyroVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softtech.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitirmeprojesiAyroVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
