84a6caec43a1b244c4b881ad6f8f4beb1475f6a0
Mertcan Elçi <mertcanelci@outlook.com>
2022-03-25 00:19:42 +0300

    Builder for UserPatchDto for tests

--- src/main/java/com/softtech/webapp/user/dto/UserPatchDto.java
+++ src/main/java/com/softtech/webapp/user/dto/UserPatchDto.java
@@ -1,8 +1,10 @@
 package com.softtech.webapp.user.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserPatchDto {
     private String username;
     private String name;
